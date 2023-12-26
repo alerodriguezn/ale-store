2fab6d623d014382c389c6d85bc1b1ed09bd9e6e
alerodriguezn <navarrocmo@gmail.com>
2023-12-26 17:10:38 -0600

    feat: slideshow desktop

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,39 @@
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+
+
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
-export default function CategoryPage({ params }: Props) {
-  const { id } = params;
-  //not found handler
+const products = initialData.products;
 
-  if (id === "kids") {
-    notFound();
+export default async function CategoryPage({ params }: Props) {
+  const { id } = params;
+
+
+  const categoryProducts = products.filter((product) => product.gender === id); 
+
+  const labels:Record<Category, string>   = {
+    'men' : 'Mens',
+    'women' : 'Womens',
+    'kid' : 'Kids',
+    'unisex': 'For All'
   }
 
+  // if (id === "kids") {
+  //   notFound();
+  // }
+
   return (
-    <div>
-      <h1>Hello Page {id}</h1>
-    </div>
+    <>
+    <Title title={labels[id]} subtitle={`All products`} className="mb-2 capitalize" />
+    <ProductGrid products={categoryProducts} />
+  </>
   );
 }
